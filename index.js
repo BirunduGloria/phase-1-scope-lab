180362429c403de44b7061d0aed26ada35ade196
Gloria Birundu <birundugloria@gmail.com>
2025-06-29 06:07:17 +0300

    complete scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Write your so// Declare in global scope using var
+var customerName = 'bob';
+
+// Uppercase the global customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Declare bestCustomer in global scope from inside a function
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // DO NOT use var/let/const
+}
+
+// Overwrite the global bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = 'someone';
+
+// Try to change the constant (this will throw an error if called)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
+
+// Export for Jest
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavoriteCustomer,
+  changeLeastFavoriteCustomer
+};
